fd7798e90cdf306592cdb131764dbfa7dbcf429f
JALG22018 <lg22018@ues.edu.sv>
2024-04-27 23:41:31 -0600

    subiendo fuentes de aplicación

--- js/script.js
+++ js/script.js
@@ -15,3 +15,24 @@ function SendMail() {
         });
 }
 
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_i0prtn8';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
