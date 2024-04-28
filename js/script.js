ec2cf374af1705437f000ff5ce14ee477a52b4ad
JALG22018 <lg22018@ues.edu.sv>
2024-04-27 23:31:57 -0600

    subiendo fuentes de aplicación

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 
+function SendMail() {
+    var params = {
+        from_name: document.getElementById("fullName").value,
+        email_id: document.getElementById("email_id").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send("service_mr14mef", "template_i0prtn8", params)
+        .then(function(res) {
+            alert("¡Éxito! " + res.status);
+        })
+        .catch(function(error) {
+            console.error('Error al enviar el correo:', error);
+            alert("Ha ocurrido un error al enviar el correo. Por favor, inténtalo de nuevo más tarde.");
+        });
+}
+
