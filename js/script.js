2a2bfa0e3f8fe495962ada09e31999444824e247
JALG22018 <lg22018@ues.edu.sv>
2024-04-27 23:37:00 -0600

    subiendo fuentes de aplicación

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// script.js
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/662c9684a0c6737bd1315e40/1hshfj2nc';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-
 function SendMail() {
     var params = {
         from_name: document.getElementById("fullName").value,
